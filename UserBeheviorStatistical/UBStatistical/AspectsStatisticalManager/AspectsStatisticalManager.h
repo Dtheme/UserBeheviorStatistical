ef09e8fa4171d242f5b58df9901bc998298d240a
duanzhiwei <armchannel_dzw@163.com>
2019-01-09 17:47:09 +0800

    update

--- UserBeheviorStatistical/UBStatistical/AspectsStatisticalManager/AspectsStatisticalManager.h
+++ UserBeheviorStatistical/UBStatistical/AspectsStatisticalManager/AspectsStatisticalManager.h
@@ -18,7 +18,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)runMonitorWithConfiguration:(NSString *)plistFileName;
 
-- (NSDictionary *)getDataFromLocalStorage;
+- (NSString *)getDataFromLocalStorage;
 
 - (void)cleanUpLocalStorage;
 @end
--- UserBeheviorStatistical/UBStatistical/AspectsStatisticalManager/AspectsStatisticalManager.m
+++ UserBeheviorStatistical/UBStatistical/AspectsStatisticalManager/AspectsStatisticalManager.m
@@ -7,7 +7,7 @@
 //
 
 #import "AspectsStatisticalManager.h"
-
+#import <objc/runtime.h>
 @implementation AspectsStatisticalManager
 
 + (instancetype)shareManager{
@@ -24,14 +24,59 @@
     [UBS_Monitor statisticEventWith:plistFileName EventKey:kLiftCycleTrack];
 }
 
-- (NSDictionary *)getDataFromLocalStorage{
+- (NSString *)getDataFromLocalStorage{
     
-    return [UBS_Storage getData];
-    
+    /*
+     {
+     EventTrack =     (
+     "<EventTrackModel: 0x6000001bcc90>",
+     "<EventTrackModel: 0x6000001ba3a0>",
+     );
+     LiftCycleTrack =     (
+     "<LifeCycleTrackModel: 0x600001a648c0>",
+     "<LifeCycleTrackModel: 0x600001a64e80>"
+     );
+     */
+    NSDictionary *dict = [UBS_Storage getData];
+    NSMutableArray *ETs = [NSMutableArray array];
+    NSMutableArray *LCTs = [NSMutableArray array];
+    NSMutableDictionary *resultDic = [NSMutableDictionary dictionary];
+    for (EventTrackModel* model in dict[kEventTrack]) {
+        NSDictionary * modelDic = [self dicFromObject:model];
+        [ETs addObject:modelDic];
+        [resultDic setObject:ETs forKey:kEventTrack];
+    }
+    for (LifeCycleTrackModel *model in dict[kLiftCycleTrack]) {
+        NSDictionary * modelDic = [self dicFromObject:model];
+        [LCTs addObject:modelDic];
+        [resultDic setObject:LCTs forKey:kLiftCycleTrack];
+    }
+
+     NSError *parseError = nil;
+     NSData *jsonData = [NSJSONSerialization dataWithJSONObject:resultDic options:NSJSONWritingPrettyPrinted error:&parseError];
+     NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    return [jsonString copy];
 }
 
 - (void)cleanUpLocalStorage{
     [UBS_Storage resetDataBase];
 }
 
+
+
+- (NSDictionary *)dicFromObject:(id)object {
+    NSMutableDictionary *dic = [NSMutableDictionary dictionary];
+    unsigned int count;
+    objc_property_t *propertyList = class_copyPropertyList([object class], &count);
+    
+    for (int i = 0; i < count; i++) {
+        objc_property_t property = propertyList[i];
+        const char *cName = property_getName(property);
+        NSString *name = [NSString stringWithUTF8String:cName];
+        NSObject *value = [object valueForKey:name];//valueForKey返回的数字和字符串都是对象
+        
+        value == nil?[dic setObject:[NSNull null] forKey:name]:[dic setObject:[self dicFromObject:value] forKey:name];
+    }
+    return [dic copy];
+}
 @end
